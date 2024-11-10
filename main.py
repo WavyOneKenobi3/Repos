40f2e1320967e09aa8945e00eb28852a0e104e7c
Mark <19152780+WavyOneKenobi3@users.noreply.github.com>
2024-11-09 23:00:27 -0500

    commit readme file and main code

---
+++ main.py
@@ -0,0 +1,29 @@
+# title of project (To-Do list)
+print("To-Do List\n")
+
+#array for the the to-do list 
+Task = []
+addTask = str()
+addDate = float()
+ 
+def toDoList():
+    newVaules == "Y"
+    while newVaules == "Y":
+        try:
+            addTask = str(input(" Input Task "))      #user input
+            addDate = float(input("date of Task "))     #date task is due
+            Task.append(addTask)                        #append user input to array
+            Task.append(addDate)
+        except: 
+            print("ERROR")
+        else:    
+            print(Task)         
+    newVaules = input("Do you wnant to add another Task? Y or N ")
+        if newVaules == "Y" or "y":
+            continue
+        elif newVaules == "N" or "n":
+            break 
+    
+toDoList()
+#delete item off list (in array)
+
