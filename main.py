cd45939194e0321a61858a7af84b8bd10fde5ce1
Mark <19152780+WavyOneKenobi3@users.noreply.github.com>
2025-01-03 15:53:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,68 @@
+import sys
+
 # title of project (To-Do list)
 print("To-Do List\n")
+print("Welcome to Your To-Do list app. ")
+print("Select an option")
 
 #array for the the to-do list 
-Task = []
-addTask = str()
-addDate = float()
+To_Do_List = []
+
  
-def toDoList():
-    keep_Going = True
+ #Add item to list (in array)
+def add_list():
+    task = input("Add Task\n ")
+    Date_Task = input("Add Date\n ")
+    To_Do_List.append({"Task": task, "Date": Date_Task})
+    newVaules = input("Do you wnant to add another Task? Y or N ")
+    if newVaules.lower() == "n":
+        quit_list()
     
-    while keep_Going:
+ #delete item off list (in array)
+def delete_list():
+    task = input("Delete Task: ")
+    Date_Task = input("Delete Date: ")
+    item_to_remove = {"Task": task, "Date": Date_Task}
+    if item_to_remove in To_Do_List:
+        To_Do_List.remove(item_to_remove)
+        print("Task removed.")
+    else:
+        print("Task not found in the list.")
+    newVaules = input("Do you wnant to add another Task? Y or N ")
+    if newVaules.lower() == "n":
+        quit_list()
+ 
+ #Display list
+def display_list():
+    print(To_Do_List)
+    newVaules = input("Do you wnant to add another Task? Y or N ")
+    if newVaules.lower() == "n":
+        quit_list()
+    
+def quit_list():
+    print("Ending program")
+    sys.exit()
+
+def play():  
+    while True:
         try:
-            addTask = str(input(" Input Task "))      #user input
-            addDate = float(input("date of Task "))     #date task is due
-            Task.append(addTask)                        #append user input to array
-            Task.append(addDate)
+            user_input = input("1. Add Task \n2. Delete Task\n3. Show List\n4. Quit\n").strip() 
+            if user_input == "1":
+                add_list()
+            elif user_input == "2":
+                delete_list()
+            elif user_input == "3":
+                display_list()
+            elif user_input == "4":
+                quit_list()
+            else:
+                print("Invalid option. Please try again.")
         except: 
             print("ERROR")
-        else:    
-            print(Task)         
-        newVaules = input("Do you wnant to add another Task? Y or N ")
-        if newVaules == "Y" or "y":
-            continue
-        elif newVaules == "N" or "n":
-            break 
-    
-toDoList()
-#delete item off list (in array)
+            
+
+
+
+play()
+
 
