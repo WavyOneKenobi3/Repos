6b6cf49bf034a2bbf52c12fa7b478324290f96ca
Mark <19152780+WavyOneKenobi3@users.noreply.github.com>
2025-01-05 19:43:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,39 @@
-import sys
-
 # title of project (To-Do list)
+
+#array for the the to-do list 
+To_Do_List = {}
 print("To-Do List\n")
 print("Welcome to Your To-Do list app. ")
 print("Select an option")
 
-#array for the the to-do list 
-To_Do_List = []
 
  
  #Add item to list (in array)
 def add_list():
     task = input("Add Task\n ")
     Date_Task = input("Add Date\n ")
-    To_Do_List.append({"Task": task, "Date": Date_Task})
-    newVaules = input("Do you wnant to add another Task? Y or N ")
-    if newVaules.lower() == "n":
-        quit_list()
+    To_Do_List.append({"Task": task, "Date": Date_Task})    
     
  #delete item off list (in array)
 def delete_list():
     task = input("Delete Task: ")
     Date_Task = input("Delete Date: ")
     item_to_remove = {"Task": task, "Date": Date_Task}
-    if item_to_remove in To_Do_List:
+    try: 
+        if item_to_remove in To_Do_List:
         To_Do_List.remove(item_to_remove)
         print("Task removed.")
     else:
         print("Task not found in the list.")
-    newVaules = input("Do you wnant to add another Task? Y or N ")
-    if newVaules.lower() == "n":
-        quit_list()
- 
+    except: KeyError:
+        print("Not in list")
+        pass
+    
  #Display list
 def display_list():
     print(To_Do_List)
     newVaules = input("Do you wnant to add another Task? Y or N ")
-    if newVaules.lower() == "n":
-        quit_list()
+    
     
 def quit_list():
     print("Ending program")
@@ -54,7 +50,8 @@ def play():
             elif user_input == "3":
                 display_list()
             elif user_input == "4":
-                quit_list()
+                print("Ending Program")
+                break
             else:
                 print("Invalid option. Please try again.")
         except: 
